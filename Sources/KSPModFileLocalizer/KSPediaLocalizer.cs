6a773cceb46d16b4c2efa0bc231505ff0f04ee18
Nils Büscher <nils.buescher@uni-rostock.de>
2021-04-15 21:08:23 +0200

    Update for KSP 1.11.2

--- Sources/KSPModFileLocalizer/KSPediaLocalizer.cs
+++ Sources/KSPModFileLocalizer/KSPediaLocalizer.cs
@@ -8,7 +8,7 @@ namespace KSPediaLocalizer
     [KSPAddon(KSPAddon.Startup.Instantly, true)]
     public class KSPediaLocalizer : MonoBehaviour
     {
-        string[] Languages = { "en-us", "es-es", "es-mx", "ja", "ru", "zh-cn" };
+        string[] Languages = { "en-us", "es-es", "es-mx", "ja", "ru", "zh-cn", "de-de", "it-it", "fr-fr", "pt-br"};
 
         private ConfigNode[] nodes;
 
@@ -21,6 +21,8 @@ namespace KSPediaLocalizer
 
             string currentLanguage = Localizer.CurrentLanguage;
 
+            Debug.Log("[KSPediaLocalizer] Current language is: " + currentLanguage);
+
             //try to get the config node
             try
             {
@@ -28,7 +30,8 @@ namespace KSPediaLocalizer
             }
             catch (Exception e)
             {
-                Debug.LogError("[KSPediaLocalizer] ERROR config exception: " + e.Message);
+                Debug.LogError("[KSPediaLocalizer] Config exception: " + e.Message);
+                Debug.LogException(e);
             }
 
             for (int nodeID = 0; nodeID < nodes.Length; nodeID++)
@@ -39,26 +42,35 @@ namespace KSPediaLocalizer
                     string fileName = nodes[nodeID].GetValue("filename");
                     string defaultLang = nodes[nodeID].GetValue("default");
 
-                    string setLanguage = string.Empty;
+                    string setLanguage = currentLanguage;
 
                     //check of the right language is already set
                     if (!File.Exists(filePath + fileName + "_" + currentLanguage + ".ksp"))
                     {
+                        Debug.Log("[KSPediaLocalizer] Right language is NOT set: " + filePath + fileName + "_" + currentLanguage + ".ksp");
+
                         //when the right language can be set
                         if (File.Exists(filePath + fileName + "_" + currentLanguage + ".lang")) {
+                            Debug.Log("[KSPediaLocalizer] Setting language to: " + filePath + fileName + "_" + currentLanguage + ".ksp");
                             File.Move(filePath + fileName + "_" + currentLanguage + ".lang", filePath + fileName + "_" + currentLanguage + ".ksp");
                             setLanguage = currentLanguage;
                         }
                         //else set to the default language
                         else if (File.Exists(filePath + fileName + "_" + defaultLang + ".lang"))
                         {
+                            Debug.LogError("[KSPediaLocalizer] Cannot find translation file for " + currentLanguage);
+                            Debug.Log("[KSPediaLocalizer] Setting default language to: " + filePath + fileName + "_" + defaultLang + ".ksp");
                             File.Move(filePath + fileName + "_" + defaultLang + ".lang", filePath + fileName + "_" + defaultLang + ".ksp");
                             setLanguage = defaultLang;
                         }
                         else
                         {
-                            Debug.LogError("[KSPediaLocalizer] ERROR cannot switch to correct or default language, maybe a file is missing?");
+                            Debug.LogError("[KSPediaLocalizer] Cannot switch to correct or default language, maybe a file is missing?");
                         }
+                    }
+                    else
+                    {
+                        Debug.Log("[KSPediaLocalizer] Right language is set: " + filePath + fileName + "_" + currentLanguage + ".ksp");
                     }
 
                     //remove the old file when a language is set
@@ -70,10 +82,12 @@ namespace KSPediaLocalizer
                             {
                                 if (File.Exists(filePath + fileName + "_" + Languages[i] + ".lang"))
                                 {
+                                    Debug.Log("[KSPediaLocalizer] Deleting file : " + filePath + fileName + "_" + Languages[i] + ".ksp");
                                     File.Delete(filePath + fileName + "_" + Languages[i] + ".ksp");
                                 }
                                 else
                                 {
+                                    Debug.Log("[KSPediaLocalizer] Renaming file : " + filePath + fileName + "_" + Languages[i] + ".ksp");
                                     File.Move(filePath + fileName + "_" + Languages[i] + ".ksp", filePath + fileName + "_" + Languages[i] + ".lang");
                                 }
                             }
@@ -82,7 +96,8 @@ namespace KSPediaLocalizer
                 }
                 catch (Exception e)
                 {
-                    Debug.LogError("[KSPediaLocalizer] ERROR switching KSPedia files. Please make sure that KSP has full file access to its directories: " + e.Message);
+                    Debug.LogError("[KSPediaLocalizer] Error while switching KSPedia files. Please make sure that KSP has full file access to its directories: " + e.Message);
+                    Debug.LogException(e);
                 }
             }
         }
